e5947d276d71043a4c6b6906c8df4ead7dccd0a5
ErickaEmy <erimartinezy@upt.pe>
2025-11-30 10:40:36 -0500

    a

---
+++ SistemaLaboratorio/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaLaboratorio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}

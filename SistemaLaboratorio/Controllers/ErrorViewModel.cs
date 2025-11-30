606f2a134ad78af4efff32f8b60654b1e4fe93a8
ErickaEmy <erimartinezy@upt.pe>
2025-11-30 10:34:44 -0500

    actualizacion home

---
+++ SistemaLaboratorio/Controllers/ErrorViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SistemaLaboratorio.Models;
+
+namespace SistemaLaboratorio.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error")]
+        public IActionResult Index()
+        {
+            // Obtiene los detalles del error
+            var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            var model = new ErrorViewModel
+            {
+                RequestId = HttpContext.TraceIdentifier,
+                ErrorMessage = exceptionFeature?.Error.Message,
+                StackTrace = exceptionFeature?.Error.StackTrace
+            };
+
+            return View("Error", model);
+        }
+    }
+}
